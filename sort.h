bedbb52789f202faea515e16fe3c28daed727e70
Haylemicheal Berihun <haylemicheal.mekonnen@gmail.com>
2022-07-18 22:01:01 +0300

    Add bubble sort

--- sort.h
+++ sort.h
@@ -1,5 +1,6 @@
 #ifndef SORT_H
 #define SORT_H
+#include <stddef.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -10,11 +11,12 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+void bubble_sort(int *array, size_t size);
 #endif
